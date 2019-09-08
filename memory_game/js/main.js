24f87d16d06705a21360425bb4ec9c8d9bdc5e06
Steven Wolanin Villanueva <stevenwolanin@stevens-imac.lan>
2019-09-08 16:14:20 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+console.log(" up and running!");
+var cardOne = "Queen";
+var cardTwo = "king";
+var cardThree = "Queen";
+var cardFour = "King"
+
+console.log("user flipped" + cardOne);
+console.log("user flipped" + cardTwo);
