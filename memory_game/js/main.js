3050e69fc296cd9e390684b48001680343628bdd
Steven Wolanin Villanueva <stevenwolanin@Stevens-iMac.local>
2019-09-09 23:47:34 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,50 @@
-const cards = ["queen", "queen", "king", "king"];
+
+
+
+
+
+var cards = [
+{rank: "queen", suit: "hearts", cardImage: "images/queen-of-hearts.png"},
+{rank: "queen", suit: "diamonds", cardImage: "images/queen-of-diamonds.png"},
+{rank: "king", suit: "hearts", cardImage: "images/king-of-hearts.png"},
+{rank: "king", suit: "diamons", cardImage: "images/king-of-diamonds.png"},
+];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("user flipped queen");
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped king");
-
-if (cardsInPlay.length === 2 && cardOne === cardTwo){
-		alert("you found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
+function checkForMatch() {
+	setAttribute(cards[cardId].cardImage) // I thin'k this part is wrong.
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}	
+}
 
 
+function flipCard() {
+var carId = this.getAttribute('data-id');
+cardsInPlay.push(cards[cardid].rank);
 
+console.log("user flipped " + cards[cardId].rank);
+console.log("user flipped" + cards[cardId].cardImage);
+console.log("user flipped" + caeds[cardId].suit);
+
+checkForMatch();
+}
+
+
+
+ function createBoard() {
+  const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++) {
+  var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+
+  }
+};
+
+createBoard();
